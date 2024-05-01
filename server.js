ccad48109d92eaf7d8a8e860b24cc2d13cc37133
Bradley Blue <b_blue@mail.fhsu.edu>
2024-04-30 21:00:24 -0500

    Change to port value for testing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const router = require('./routes/subdir');
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.DATABASE_URI || 3500;
 
 app.use(cors());
 
@@ -26,7 +26,7 @@ app.use(router);
 
 // Listen for port connection
 mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');;
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
