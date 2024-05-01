501eeba9db72fcc1f7cc524998cca417e92033e0
Bradley Blue <b_blue@mail.fhsu.edu>
2024-04-30 20:02:38 -0500

    Working project without checking tests

--- controllers/funFactController.js
+++ controllers/funFactController.js
@@ -1,3 +1,4 @@
+const States = require('../model/States');
 const StateFunFacts = require('../model/States');
 
 const statesData = {
@@ -15,18 +16,6 @@ const randomFunFact = async (req, res) => {
     return res.json({'funfact': factsArray[Math.floor(Math.random() * len)]});
 };
 
-/* const getStateFunFacts = async (req, res ) => {
-    const found = statesData.states.find((state) => {
-        return state.code === req.params.code.toUpperCase();
-   });
-
-    const facts = await StateFunFacts.find({ stateCode: req.params.code.toUpperCase() }, 'funfacts').exec();
-    const factsArray = facts.map(fact => fact.funfacts);
-
-    found['funfacts'] = factsArray;
-    res.json(found);
-}; */
-
 const getStateFunFactsItem = async (code) => {
 
     const facts = await StateFunFacts.find({ stateCode: code}, 'funfacts').exec();
@@ -37,4 +26,66 @@ const getStateFunFactsItem = async (code) => {
     return factsObj;
 };
 
-module.exports = { randomFunFact, getStateFunFactsItem };
+const postStateFunFact = async (req, res) => {
+    if (!req?.body?.funfacts) {
+        return res.status(400).json({ 'message': 'Funfact is required'});
+    }
+
+    try {
+        const result = await States.updateOne({
+            stateCode: req.params.code.toUpperCase()},
+            {$push: {funfacts: req.body.funfacts}},
+            {upsert: true}
+        );
+        res.status(201).json(result);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const patchStateFunFact = async (req, res) => {
+    if (!req?.body?.funfacts || !req?.body?.index) {
+        return res.status(400).json({ 'message': 'Funfact and index are required'});
+    }
+
+    try {
+        const index = parseInt(req.body.index) - 1;
+
+        const result = await States.updateOne({
+            stateCode: req.params.code.toUpperCase()},
+            {$set: {[`funfacts.${index}`]: req.body.funfacts}}
+        );
+
+        res.status(201).json(result);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const deleteStateFunFact = async (req, res) => {
+    if (!req?.body?.index) {
+        return res.status(400).json({ 'message': 'Index is required'});
+    }
+
+    try {
+        const index = parseInt(req.body.index) - 1;
+
+        const result = await States.updateOne(
+            {stateCode: req.params.code.toUpperCase()},
+            {$set: {[`funfacts.${index}`]: null}}
+        );
+
+        if (result.modifiedCount) {
+            await States.updateOne(
+                {stateCode: req.params.code.toUpperCase()},
+                {$pull: {funfacts: null}}      
+            )
+        };
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports = { randomFunFact, getStateFunFactsItem, postStateFunFact, patchStateFunFact, deleteStateFunFact };
--- routes/subdir.js
+++ routes/subdir.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { getState, statesData, getStates, stateCapital, stateNickname, statePop, stateAdmission } = require('../controllers/statesController');
 const statesController = require('../controllers/statesController');
 const funFactController = require('../controllers/funFactController');
-const randomFunFact = require('../controllers/funFactController');
+const { randomFunFact, postStateFunFact, patchStateFunFact, deleteStateFunFact } = require('../controllers/funFactController');
 
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
@@ -15,7 +15,11 @@ router.route('/states').get(statesController.getStates);
 
 router.route('/states/:code').get(statesController.getState);
 
-router.route('/states/:code/funfact').get(funFactController.randomFunFact);
+router.route('/states/:code/funfact')
+    .get(funFactController.randomFunFact)
+    .post(funFactController.postStateFunFact)
+    .patch(funFactController.patchStateFunFact)
+    .delete(funFactController.deleteStateFunFact);
 
 router.route('/states/:code/capital').get(statesController.stateCapital);
 
